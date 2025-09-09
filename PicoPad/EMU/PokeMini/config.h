4a17acb2d32d1c11a15440ef76bd844297e30886
Gand46 <90488276+Gand46@users.noreply.github.com>
2025-09-08 21:37:14 -0500

    Include default config for PokeMini

--- PicoPad/EMU/PokeMini/config.h
+++ PicoPad/EMU/PokeMini/config.h
@@ -1,9 +1,16 @@
 #ifndef _CONFIG_H
 #define _CONFIG_H
 
+// *********
+// Here you can override default options from config_def.h.
+// *********
+
 // Configure built-in ST7789 LCD for 240x240 resolution
 #define USE_ST7789 1
 #define WIDTH 240
 #define HEIGHT 240
 
-#endif
+// Include library default configuration
+#include CONFIG_DEF_H
+
+#endif // _CONFIG_H

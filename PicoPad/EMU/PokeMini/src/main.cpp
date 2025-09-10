32fa701d94e958384259481a427fe8fb367ecbf5
Gand46 <90488276+Gand46@users.noreply.github.com>
2025-09-09 20:45:12 -0500

    Add skeleton PokéMini emulator port

---
+++ PicoPad/EMU/PokeMini/src/main.cpp
@@ -0,0 +1,15 @@
+#include "../include.h"
+
+void EmulatorLoop(void)
+{
+    // Initialize emulator (placeholder)
+    PokeMini_Reset(1);
+    // TODO: integrate display, input and sound similar to GBC/NES ports
+}
+
+int main()
+{
+    EmulatorLoop();
+    return 0;
+}
+
---
+++ PicoPad/EMU/PokeMini/src/main.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void EmulatorLoop(void);
+
+#ifdef __cplusplus
+}
+#endif
+
